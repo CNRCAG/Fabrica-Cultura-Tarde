6cd419e336be1a6f0eb2996fe1f66dfc7813e60d
Cnrc <cristhianoag@gmail.com>
2026-03-28 14:32:19 -0300

    Jogo-Fabrica
    Jogo da fabrica

---
+++ olá-mundo-[cristhiano]-[sab---tarde]/scripts/menu.gd
@@ -0,0 +1,22 @@
+extends Panel
+
+@onready var start_button = $Iniciar 
+@onready var sair = $Sair
+
+
+func _ready():
+	start_button.grab_focus()
+	
+func _process(_delta):
+	if  start_button.is_hovered():
+		start_button.grab_focus()
+	elif sair .is_hovered():
+		sair.grab_focus()
+
+
+func _on_iniciar_pressed():
+	get_tree().change_scene_to_file("res://scenes/player.tscn")#botar a cena do jogo
+
+
+func _on_sair_pressed():
+	get_tree().quit()
